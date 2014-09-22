6b2b15f484d50f6f82f7850450781cdf44f76b69
Alan Mattson <alan@borealisdev.com>
2014-09-22 18:58:13 -0400

    Test code to add temp values to database

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ __author__ = 'alan'
 import os
 import glob
 import time
+import sqlite3
+from datetime import datetime
 
-#Commenting these out as they will be run at boot time
+# Commenting these out as they will be run at boot time
 #os.system('modprobe w1-gpio')
 #os.system('modprobe w1-therm')
 
+#Database Creation and Connection Setup
+dbconnection = sqlite3.connect('temperatures.db')
+dbcursor = dbconnection.cursor()
+dbcursor.execute("CREATE TABLE IF NOT EXISTS temps(date timestamp, device1 int, device2 int, device3 int)")
+dbconnection.commit()
+dbconnection.close()
 
 # Add temp probe folder names from /esys/bus/w1/devices/
 temp_probe1 = "28-001415ab5aff"
@@ -31,53 +39,59 @@ def read_temp_raw(current_file):
     f.close()
     return lines
 
+
 #Read all of the temperature files specified in the device_list
 def read_multiple_temp():
-#Create Dictionary to store current temp readings
+    #Create Dictionary to store current temp readings
     values = {}
-#Iterate through the temperature probes
+    #Iterate through the temperature probes
     for item in device_list:
         device_file = base_dir + item + '/w1_slave'
         lines = read_temp_raw(device_file)
-#Check for valid output from hardware probe when reading by checking CRC
+        #Check for valid output from hardware probe when reading by checking CRC
         while lines[0].strip()[-3:] != 'YES':
             time.sleep(0.2)
             lines = read_temp_raw()
         equals_pos = lines[1].find('t=')
         if equals_pos != -1:
-            temp_string=lines[1][equals_pos+2:]
-            temp_c = float(temp_string) / 1000.0
+            temp_store = int(lines[1][equals_pos + 2:equals_pos + 7])
         else:
-            temp_c = 0.000
-        values[item]=temp_c
+            temp_c = 0000
+        values[item] = temp_store
     return values
 
 
-
-
-
-
-
-
-
 while True:
-    current_temps =  read_multiple_temp()
+    current_temps = read_multiple_temp()
     print current_temps
-    time.sleep(1)
+    dbconnection = sqlite3.connect('temperatures.db')
+    dbcursor = dbconnection.cursor()
+    now = datetime.now()
+    #dbcursor.execute(
+    #    '''INSERT INTO temps(device1, device2, device3) VALUES(?,?,?)''', (temp_value1, temp_value2, temp_value3))
+    # Real execute commented out for testing
+    dbcursor.execute(
+     '''INSERT INTO temps(date,device1, device2, device3) VALUES(?,?,?,?)''', (now, current_temps['device1'],
+                                                                        current_temps['device2'],
+                                                                        current_temps['device3']))
+    dbconnection.commit()
+    dbconnection.close()
+
+    time.sleep(5)
 
 
 
 
 
 
-# def read_temp():
-#     lines = read_temp_raw()
-#     while lines.strip()[-3:] != 'YES':
-#         time.sleep(0.2)
-#         lines = read_temp_raw()
-#     equals_pos = lines[1].find('t=')
-#     if equals_pos != -1:
-#         temp_string = lines[1][equals_pos+2:]
-#         temp_c = float(temp_string) / 1000.0
-#         temp_f = temp_c * 9.0 / 5.0 + 32.0
-#         return temp_c, temp_f
+    # def read_temp():
+    #     lines = read_temp_raw()
+    #     while lines.strip()[-3:] != 'YES':
+    #         time.sleep(0.2)
+    #         lines = read_temp_raw()
+    #     equals_pos = lines[1].find('t=')
+    #     if equals_pos != -1:
+    #         temp_string = lines[1][equals_pos+2:]
+    #         temp_c = float(temp_string) / 1000.0
+    #         temp_f = temp_c * 9.0 / 5.0 + 32.0
+    #         return temp_c, temp_f
