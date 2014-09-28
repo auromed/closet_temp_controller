9caf5628c426b8a41f0caa6d372ea2da5252f120
Alan Mattson <alan@borealisdev.com>
2014-09-28 15:22:56 -0400

    updated temp_probe naming between test and production to enable fewer changes when starting in production

--- graphing.py
+++ graphing.py
@@ -21,20 +21,21 @@ for row in dbcursor.execute(sql):
     splitInfo = startingInfo.split(',')
     graphArrayAppend = splitInfo[0]+','+splitInfo[1]+','+splitInfo[2]+','+splitInfo[3]
     graphArray.append(graphArrayAppend)
+    print graphArray
 
 dbconnection.commit()
 dbconnection.close()
 
 datestamp, value1, value2, value3 = np.loadtxt(graphArray,delimiter=',', unpack=True,
-                              converters={ 0: mdates.strpdate2num('%Y-%m-%d %H:%M:%S')})
-
-
+                              converters={0:mdates.strpdate2num('%Y-%m-%d %H:%M:%S')})
+print datestamp
+print mdates.num2date(datestamp)
 
 fig = plt.figure()
 ect = fig.patch
-print datestamp
 ax1 = fig.add_subplot(1,1,1, axisbg='white')
 plt.plot_date(x=datestamp, y=value1/10, fmt='b-', label = 'value', linewidth=2)
 plt.plot_date(x=datestamp, y=value2/10, fmt='r-', linewidth=2)
 plt.plot_date(x=datestamp, y=value3/10, fmt='g-', linewidth=2)
 plt.show()
+#plt.savefig('test.png')
